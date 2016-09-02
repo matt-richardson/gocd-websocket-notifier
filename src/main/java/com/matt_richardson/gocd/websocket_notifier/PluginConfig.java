45b40a03f8fba5ecc9a66a0e615668da6dc3dd5a
michelle.chen <michelle.chen@visioncritical.com>
2016-09-02 11:22:36 -0700

    corrected negation on method checking if host is set

--- src/main/java/com/matt_richardson/gocd/websocket_notifier/PluginConfig.java
+++ src/main/java/com/matt_richardson/gocd/websocket_notifier/PluginConfig.java
@@ -34,5 +34,5 @@ public class PluginConfig {
     
     public String getHost() { return host; }
     public void setHost(String host) { this.host = host; }
-    public boolean isHostSet() { return host.isEmpty(); }
+    public boolean isHostSet() { return !host.isEmpty(); }
 }
